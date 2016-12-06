d5cb0ae03d47129d90839f7f568c0f132597e75c
nampt <nampt282@gmail.com>
2016-12-06 14:11:03 +0800

    [toto_singapore] initialize files

---
+++ Shell/toto_singgapore/toto.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+get_data_webpage(){
+  local url="$1"
+  local output="$2"
+  curl $url -o $output
+}
+
+get_winner_numbers(){
+
+  local url="$1"
+
+  get_data_webpage $url nam.html
+
+  for i in {1..6}
+  do
+    awk /win${i}/'{split($0,tmp1,">");split(tmp1[2],tmp2,"<");print tmp2[1]}' nam.html 
+  done
+
+}
+
+get_winner_numbers $@
