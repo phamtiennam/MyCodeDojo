48bf00d098446bfa36e1a2a249006a6e8831545d
nampt <nampt282@gmail.com>
2016-12-07 11:43:49 +0800

    [upwork_dhcpd.conf]: release version 1.0

--- Shell/upwork_dhcpd.conf/generate_dhcpd.conf.bash
+++ Shell/upwork_dhcpd.conf/generate_dhcpd.conf.bash
@@ -27,4 +27,75 @@ list_valid_mac(){
   done < listmac.txt
 }
 
-list_valid_mac
+ping_range_of_ip(){
+
+  local gateway_ip="$(ip route|awk '/default/{print $3}')"
+  local subnet="24"
+
+  [ "$gateway_ip" != "" ]&& nmap -sP ${gateway_ip}/${subnet}
+}
+
+create_dhcpd.conf(){
+
+local router_gw="$(ip route|awk '/default/{print $3}')"
+local j=120
+local subnet="255.255.254.0"
+local domain_name="cse.edu"
+local domain_name_servers="139.182.2.1, 139.182.154.13"
+local time_offset=-28800
+local default_lease_time=21600
+local max_lease_time=43200
+
+cat <<EOF > dhcpd.conf
+#
+# DHCP Server Configuration file.
+#  see /usr/share/doc/dhcp*/dhcpd.conf.sample
+#
+ddns-update-style interim;
+ignore client-updates;
+
+subnet $router_gw netmask $subnet {
+  option routers                  $router_gw;
+  option subnet-mask              $subnet;
+
+  option domain-name              "$domain_name";
+  option domain-name-servers      $domain_name_servers;
+
+  option time-offset              $time_offset;
+
+  default-lease-time              $default_lease_time;
+  max-lease-time                  $max_lease_time;
+}
+
+EOF
+
+[ ! -f listmac.txt ]&& exit 0
+
+while read mac
+do
+
+  local ip="$(echo $router_gw|awk -F "." '{OFS=".";print $1,$2,$3}').$j"
+
+  cat <<EOF >>dhcpd.conf
+host coyote-$j {
+  hardware ethernet $mac;
+  fixed-address ${ip};
+}
+
+EOF
+
+  j=$((j+1))
+
+done < listmac.txt
+
+}
+
+#######################Main##################################
+
+_main_(){
+  ping_range_of_ip
+  list_valid_mac
+  create_dhcpd.conf
+}
+
+_main_
