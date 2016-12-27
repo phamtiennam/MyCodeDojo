e9f349e4388a0004620c823385c5b94e762db852
nampt <nampt282@gmail.com>
2016-12-27 11:24:08 +0800

    [auto_click_shaiya]:new function shaiya2

--- Shell/auto_click_shaiya/auto_click.bash
+++ Shell/auto_click_shaiya/auto_click.bash
@@ -6,12 +6,12 @@ check_xdotool(){
 
 right_click(){
 	xdotool click 3
-	sleep 1
+	#sleep 1
 }
 
 left_click(){
 	xdotool click 1
-	sleep 1
+	#sleep 1
 }
 
 mouse_move(){
@@ -78,6 +78,37 @@ shaiya(){
 		echo =====================Done===========================
 	done	
 }
+shaiya2(){
+        tail -n4 setup > tmp
+        mv tmp setup
+
+        local enter_quest=$(awk '/enter/{print $2,$3}' setup)
+        local open_quest1=$(awk '/q1/{print $2,$3}' setup)
+        local accept_quest=$(awk '/act/{print $2,$3}' setup)
+        local open_quest2=$(awk '/q2/{print $2,$3}' setup)
+
+        local times_="200"
+        local sleep_time="3"
+	while [ "$(cat stopshaiya.bash)" != "stop" ];do
+		echo =====================Start===========================
+                
+                mouse_move $open_quest1
+                multi_clicks right $times_
+                sleep $sleep_time
+                mouse_move $open_quest2
+                multi_clicks right $times_
+                sleep $sleep_time
+
+                mouse_move $enter_quest
+                multi_clicks left $times_
+                sleep $sleep_time
+                mouse_move $accept_quest
+                multi_clicks left $times_
+                sleep $sleep_time
+		echo =====================Done===========================
+	done	
+}
 #=================================Main==========================================
 check_xdotool
-shaiya
+#shaiya
+shaiya2
