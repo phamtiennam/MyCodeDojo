4ca2cb1de3c376bd1d3aefccd8c158b5c2782c91
tnpham <tn_pham@compex.com.sg>
2017-01-11 09:36:47 +0800

    [QSDK-SPF4.0]:script to build QSDK SPF_4.0

---
+++ Python/install_SPF_4.0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+import tempfile
+
+class untarpackage:
+
+  extract_dir = "qca.git"
+
+  #def __init__(self,name,path):
+  def __init__(self,name):
+    self.name = name
+    #self.path = path
+
+  def untar(self):
+    temp_dir = "temp_dir"
+    os.mkdir(temp_dir, 0777)
+    from pyunpack import Archive
+    #print(Archive(self.name).extractall(self.path))
+    Archive(self.name).extractall(temp_dir)
+    print(os.listdir(temp_dir))
+
+
+
+action = untarpackage("b161005-r00004.1-qca-networking-2016-spf-4-0_qca_oem-83ab7450f.zip")
+action.untar()
+
+#os.rename(os.listdir(temp_dir),"namtest234")
