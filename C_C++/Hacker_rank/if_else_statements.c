048bddbbff67f755d990759eeda195287dc5df90
nampt <nampt@nampts-MacBook-Air.local>
2017-04-15 20:35:54 +0800

    [Hackerrank_C]if_else_statements.Edit description

--- C_C++/Hacker_rank/if_else_statements.c
+++ C_C++/Hacker_rank/if_else_statements.c
@@ -1,13 +1,13 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Description:30 days of coding.Hackerrank.com
-//Given an integer, , perform the following conditional actions:
+//Given an integer,n , perform the following conditional actions:
 //
-//If  is odd, print Weird
-//If  is even and in the inclusive range of  to , print Not Weird
-//If  is even and in the inclusive range of  to , print Weird
-//If  is even and greater than , print Not Weird
-//Complete the stub code provided in your editor to print whether or not  is weird.
+//If n is odd, print Weird
+//If n is even and in the inclusive range of 2 to 5, print Not Weird
+//If n is even and in the inclusive range of 6 to 20, print Weird
+//If n is even and greater than 20, print Not Weird
+//Complete the stub code provided in your editor to print whether or not n is weird.
 //----------------------------
 //Author: NamPham
 //Email: nampt282@gmail.com
