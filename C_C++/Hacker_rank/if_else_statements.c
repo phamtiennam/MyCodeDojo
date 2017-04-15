fa829f79c2e65a30e1e68dd697c29859957d81f3
nampt <nampt@nampts-MacBook-Air.local>
2017-04-15 18:09:49 +0800

    [Hackerrank_C]initialize if_else_statements.c

---
+++ C_C++/Hacker_rank/if_else_statements.c
@@ -0,0 +1,22 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description:30 days of coding.Hackerrank.com
+//Given an integer, , perform the following conditional actions:
+//
+//If  is odd, print Weird
+//If  is even and in the inclusive range of  to , print Not Weird
+//If  is even and in the inclusive range of  to , print Weird
+//If  is even and greater than , print Not Weird
+//Complete the stub code provided in your editor to print whether or not  is weird.
+//----------------------------
+//Author: NamPham
+//Email: nampt282@gmail.com
+//Date: 2017_04_15
+
+#include <stdio.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	return 0;
+}
