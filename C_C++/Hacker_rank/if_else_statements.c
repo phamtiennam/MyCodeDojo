6ef2b09a824a1cce65449b599db7c4b603d21bdd
nampt <nampt@nampts-MacBook-Air.local>
2017-04-15 20:30:20 +0800

    [Hackerrank_C]if else statements

--- C_C++/Hacker_rank/if_else_statements.c
+++ C_C++/Hacker_rank/if_else_statements.c
@@ -18,5 +18,17 @@
 int main(){
 	int n;
 	scanf("%d", &n);
+	if (n%2){
+		printf("Weird");
+	}
+	else if(n>=2 & n<=5){
+		printf("Not Weird");
+	}
+	else if(n>=6 & n<=20){
+		printf("Weird");
+	}
+	else if(n>=20){
+		printf("Not Weird");
+	}
 	return 0;
 }
