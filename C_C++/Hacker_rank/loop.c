71983731328f9ef4ec36ef1a1b6d78b045b0f2dc
nampt <nampt@nampts-MacBook-Air.local>
2017-04-17 14:00:33 +0800

    [Hackerrank_C]Day5_loops

---
+++ C_C++/Hacker_rank/loop.c
@@ -0,0 +1,17 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description:Given an integer,n , print its first 10 multiples. Each multiple n*i  (where 1<= i <= 10 ) should be printed on a new line in the form: n x i = result.
+//Author: NamPham
+//Email: nampt282@gmail.com
+//Date: 2017_04_16
+#include <stdio.h>
+
+int main(){
+	int i=1,n;
+	scanf("%d", &n);
+	while(i<=10){
+		printf("%d x %d = %d\n", n, i, n*i);
+		i++;
+	}
+	return 0;
+}
