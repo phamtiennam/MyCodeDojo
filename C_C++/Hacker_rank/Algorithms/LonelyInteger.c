f25c1e648daf9f5674d34f80def4ce66805b5601
nampt <nampt@nampts-MacBook-Air.local>
2017-04-25 00:38:24 +0800

    [Hackerrank_C]Algorithms Lonely Integer

---
+++ C_C++/Hacker_rank/Algorithms/LonelyInteger.c
@@ -0,0 +1,25 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description:
+/*
+Consider an array of n integers,A=[a0,..,an-1] , where all but one of the integers occur in pairs. In other words, every element in A occurs exactly twice except for one unique element.
+
+Given A, find and print the unique element.
+
+Input Format
+
+The first line contains a single integer,n , denoting the number of integers in the array. 
+The second line contains n space-separated integers describing the respective values in A.
+
+Constraints
+1<=n<=100
+n is an odd number
+0<= ai <=100, 0<= i <= n
+
+Output Format
+
+Print the unique number that occurs only once in  on a new line.
+*/
+//Coder: NamPham
+//Email: nampt282@gmail.com
+//Date: 2017_04_25
