06428bba1ec8ce31f094c890d13d504b4e156dc2
nampt <nampt@nampts-MacBook-Air.local>
2017-05-25 08:12:08 +0800

    [Hackerrank_C++]Basic Data Types

---
+++ C_C++/Hacker_rank/C++_Intro/basic_data_types.cpp
@@ -0,0 +1,24 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description:
+//Coder: NamPham
+//Email: nampt282@gmail.com
+//Date: 2017_05_24
+#include<sstream>
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int i;
+	long l;
+	char c;
+	float f;
+	double d;
+
+	scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+	printf("%d\n%ld\n%c\n%f\n%lf", i, l, c, f, d);
+
+	return 0;
+}
+
