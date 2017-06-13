67fe9b7719eb9a4d05efa393b57a1f2e94933384
phamtiennam <tien.nam.pham-ext@continental-corporation.com>
2017-06-13 12:17:04 +0800

    [C++]Add description about virtual function.New to class

--- C_C++/virtualfunc.cpp
+++ C_C++/virtualfunc.cpp
@@ -1,6 +1,11 @@
 
 /*####################################################################
-#Description:To understand virtual function in C++
+#Description:		To understand virtual function in C++
+
+-->A virtual function is a member function in base class that you expect to redefine in derived classes.
+
+Link reference: https://www.programiz.com/cpp-programming/virtual-functions
+
 #Coder: NamPham
 #Email: nampt282@gmail.com
 #Date: 2017_06_12
@@ -13,14 +18,22 @@ using namespace std;
 class Animal
 {
 	public:
-virtual void eat() { std::cout << "I'm eating generic food.\n"; }
+	virtual void eat() { std::cout << __func__ << "I'm eating generic food."<< endl; }
 };
 
 class Cat : public Animal
 {
 	public:
-virtual void eat() { 
-		std::cout << "I'm eating a rat.\n"; 
+	void eat() { 
+		std::cout << "I'm eating a rat." << endl; 
+	}
+};
+
+class Dog : public Animal
+{
+	public:
+	void eat() { 
+		std::cout << __func__ << "I'm eating a shit." << endl; 
 	}
 };
 
@@ -30,11 +43,13 @@ void func(Animal *xyz){
 
 int main()
 {
-	Animal *animal ;
-	Cat *cat;
+	Animal *animal = new Animal;
+	Cat *cat = new Cat;
+	Dog *mydog = new Dog;
 	//animal->eat();
 	//cat->eat();
 	func(animal);
 	func(cat);
+	func(mydog);
 	return 0;
 }
