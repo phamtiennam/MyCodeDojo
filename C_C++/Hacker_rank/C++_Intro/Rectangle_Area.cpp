e661dfe0ca0106b8e7fa2ac83c921bd768d59f7c
phamtiennam <tien.nam.pham-ext@continental-corporation.com>
2017-10-11 18:32:30 +0800

    [C_C++] Practice Inheritance

---
+++ C_C++/Hacker_rank/C++_Intro/Rectangle_Area.cpp
@@ -0,0 +1,102 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description:
+/*
+Create two classes:
+
+Rectangle
+The Rectangle class should have two data fields-width and height of int types. The class should have display() method, to print the width and height of the rectangle separated by space.
+
+RectangleArea
+The RectangleArea class is derived from Rectangle class, i.e., it is the sub-class of Rectangle class. The class should have read_input() method, to read the values of width and height of the rectangle. The RectangleArea class should also overload the display() method to print the area of the rectangle.
+
+Input Format
+
+The first and only line of input contains two space separated integers denoting the width and height of the rectangle.
+
+Constraints
+
+Output Format
+
+The output should consist of exactly two lines:
+In the first line, print the width and height of the rectangle separated by space.
+In the second line, print the area of the rectangle.
+
+Sample Input
+
+10 5
+
+Sample Output
+
+10 5
+50
+*/
+
+//Coder: NamPham
+//Email: nampt282@gmail.com
+//Date: 2017_10_11
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+
+class Rectangle
+{
+public:
+	int width;
+	int height;
+	void display()
+	{
+		cout<<width<<" "<<height<<endl;
+	}
+};
+
+class RectangleArea : public Rectangle
+{
+public:
+
+	void read_input()
+	{
+		cin>>width;
+		cin>>height;
+	}
+	void display()
+	{
+		cout<<width*height<<endl;
+	}
+
+};
+
+/*
+ * ===================== Main =====================
+ */
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
